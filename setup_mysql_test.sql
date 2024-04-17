727a6a646c1fc00e4539d31f968ed9af90972079
Olusegun <ayosegun2001@gmail.com>
2024-04-17 23:49:18 +0100

    0x02. AirBnB clone - MySQL

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,6 +7,8 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grant all privileges on the database to the user
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
 
 -- Grant select privilege on performance_schema to the user
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
