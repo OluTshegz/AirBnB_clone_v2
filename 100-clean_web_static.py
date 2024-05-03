76c3cd83110158be7875c2654d08fd2a8631524e
Olusegun <ayosegun2001@gmail.com>
2024-05-03 16:06:31 +0100

    0x03. AirBnB clone - Deploy static: DevOps, Python, SysAdmin, Scripting, CI/CD

---
+++ 100-clean_web_static.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+# a Fabric script (based on the file `3-deploy_web_static.py`) that deletes out-of-date archives, using the function `do_clean`
---
+++ 101-setup_web_static.pp
@@ -0,0 +1,1 @@
+# Using Puppet to redo task #0 (`0-setup_web_static.sh`): a Bash script that sets up your web servers for the deployment of `web_static`
