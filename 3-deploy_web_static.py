750f04fb8cdf5ae337ff0994b85dfc4f877faf83
Olusegun <ayosegun2001@gmail.com>
2024-05-04 03:26:53 +0100

    0x03. AirBnB clone - Deploy static: DevOps, Python, SysAdmin, Scripting, CI/CD

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,2 +1,2 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 # Write a Fabric script (based on the file `2-do_deploy_web_static.py`) that creates and distributes an archive to your web servers, using the function `deploy`
