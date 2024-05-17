a3092a1a9f7eb1ad61ebdb63e0b47456b8c82b1d
Olusegun <ayosegun2001@gmail.com>
2024-05-17 15:39:45 +0100

    0x04. AirBnB clone - Web framework: Python, Back-end, Webserver, Flask

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module for a Flask web application
+"""
+from flask import Flask
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+
+# Define a route for the root URL '/' with strict_slashes=False
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Function to display the message "Hello HBNB!" when the root URL is accessed
+    """
+    return "Hello HBNB!"
+
+
+# Define a route for the '/hbnb' URL with strict_slashes=False
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Function to display the message "HBNB" when the '/hbnb' URL is accessed
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    # Run the Flask application on 0.0.0.0 interface, port 5000
+    app.run(host="0.0.0.0", port=5000)
