a8cb98d9321baa81c46e89b3122952066fd6acfa
Olusegun <ayosegun2001@gmail.com>
2024-05-19 00:37:09 +0100

    0x02. AirBnB clone - MySQL: Python, OOP, Back-end, SQL, MySQL, ORM, SQLAlchemy

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -64,6 +64,7 @@ class FileStorage:
         obj_key = obj.to_dict()['__class__'] + '.' + obj.id
         if obj_key in self.__objects.keys():
             del self.__objects[obj_key]
+            self.save()
 
     def close(self):
         """Call the reload method"""
